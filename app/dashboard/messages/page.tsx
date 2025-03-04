9699eec640e9d3ab671367254ae8f54bc0372ed7
YRACHEK101 <r.yahia.dev@gmail.com>
2025-03-04 20:03:29 +0000

    fix app/dashboard/messages/page.tsx

---
+++ app/dashboard/messages/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const MessagesPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Messages</h1>
+      {/* Add your messages content here */}
+    </div>
+  );
+};
+
+export default MessagesPage;
